966c99f0d374bd5c78b8d797f98e1e2343efd5dd
castellon <alexcastellon077@gmail.com>
2026-05-26 18:59:25 +0200

    fix parts

--- functions.php
+++ functions.php
@@ -6,6 +6,15 @@ add_action( 'after_setup_theme', function () {
 	add_editor_style( 'style.css' );
 } );
 
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style(
+		'nch-theme-style',
+		get_stylesheet_uri(),
+		[],
+		wp_get_theme()->get( 'Version' )
+	);
+} );
+
 add_action( 'init', function () {
 	register_block_pattern_category( 'nch', [ 'label' => __( 'NCH', 'nch-theme' ) ] );
 } );
